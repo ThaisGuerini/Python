a1e091bcd79b4c4abd52f038b9ac5cd89ab53575
ThaisGuerini <43821364+ThaisGuerini@users.noreply.github.com>
2018-10-06 19:23:25 -0300

    Add files via upload

---
+++ Exercícios aula 1.py
@@ -0,0 +1,82 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      thais
+#
+# Created:     06/10/2018
+# Copyright:   (c) thais 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#Exercício 1
+d_km=10
+t_segundos=2610
+d_mi=d_km/1.61
+d_mi
+6.211180124223602
+t_medio=t_segundos/d_mi
+t_medio
+420.21000000000004
+t_horas=t_segundos/3600
+t_horas
+0.725
+velocidade=d_mi/t_horas
+velocidade
+8.567144998929106
+
+#Exercício 2
+c=3*10**8 #velocidade da luz em m/s
+c
+300000000
+v=343 #velocidade da som em m/s
+t=3*v/(c-v)
+t
+3.430003921637817e-06
+distancia=c*t
+distancia
+1029.0011764913452 #distância em metros
+
+#Exercício 3
+a=3
+b=-4
+c=-10
+delta=b**2-4*a*c
+delta
+136
+x1=(-b+(delta)**(1/2))/(2*a)
+x1
+2.610317298281767
+x2=(-b-(delta)**(1/2))/(2*a)
+x2
+-1.2769839649484336
+
+#Exercício 4
+h=5 #altura em metros
+sombra=0.5 #em metros
+theta=sombra/h
+theta
+0.1
+
+#Exercício 5
+m=69.75
+h=1.78
+IMC=m/h**2
+IMC
+22.014265875520767
+
+m=11
+h=0.7
+IMC=m/h**2
+IMC
+22.44897959183674
+
+#Exercício 6
+h=3
+g=9.8
+v=(2*g*h)**1/2
+v
+29.400000000000002
+
+t=v/g
+t
+3.0
